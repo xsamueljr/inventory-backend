0954980e4aa69e8b1db398fc9f8ea735b8ddaad5
xsamueljr <git@xsamueljr.anonaddy.me>
2025-05-05 21:12:10 +0200

    refactor(test): updated calls to use cases whose parameters where replaced by dtos

--- tests/test_register_sell.py
+++ tests/test_register_sell.py
@@ -3,7 +3,7 @@ from typing import Tuple
 
 import pytest
 
-from products.application.register_sell import RegisterSaleUsecase
+from products.application.register_sell import RegisterSaleUsecase, SaleDTO
 from products.infrastructure.in_memory_product_repository import InMemoryProductRepository
 from tests.mocks import MockMailer
 from tests.mothers.product import ProductMother
@@ -30,7 +30,7 @@ def test_happy_path(sell_setup) -> None:
     product = ProductMother.create(stock=5)
     sell_setup.repo.save(product)
 
-    sell_setup.usecase.run(product.name, 3)
+    sell_setup.usecase.run(SaleDTO(product.id, 3))
 
     after_sale_product = sell_setup.repo.get_by_id(product.id)
 
@@ -41,6 +41,6 @@ def test_if_stock_left_zero_or_negative_then_mail_is_sent(sell_setup) -> None:
     product = ProductMother.create(stock=2)
     sell_setup.repo.save(product)
 
-    sell_setup.usecase.run(product.name, 3)
+    sell_setup.usecase.run(SaleDTO(product.id, 3))
 
     assert sell_setup.mailer.calls_count() == 2
