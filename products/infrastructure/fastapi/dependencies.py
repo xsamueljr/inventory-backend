f000591f4ff13757ec94feb471764310928f95e5
xsamueljr <git@xsamueljr.anonaddy.me>
2025-05-13 19:05:40 +0200

    feat: new endpoints, sqlite for products, CORS for frontend in the same machine...

--- products/infrastructure/fastapi/dependencies.py
+++ products/infrastructure/fastapi/dependencies.py
@@ -7,6 +7,7 @@ from emails.domain.emailer import Emailer
 from products.application.create_product import CreateProductUseCase
 from products.application.get_all import GetAllProductsUsecase
 from products.application.get_by_id import GetProductByIdUsecase
+from products.application.register_arrival import RegisterArrivalUsecase
 from products.application.register_sell import RegisterSaleUsecase
 from products.domain.product_repository import ProductRepository
 from products.infrastructure.in_memory_product_repository import InMemoryProductRepository
@@ -31,6 +32,11 @@ def get_register_sale_usecase(
         mailer: Emailer = Depends(get_mailer)
 ) -> RegisterSaleUsecase:
     return RegisterSaleUsecase(product_repository, mailer)
+
+def get_register_arrival_usecase(
+        product_repository: ProductRepository = Depends(get_product_repository)
+) -> RegisterArrivalUsecase:
+    return RegisterArrivalUsecase(product_repository)
 
 def get_all_products_usecase(repo: ProductRepository = Depends(get_product_repository)) -> GetAllProductsUsecase:
     return GetAllProductsUsecase(repo)
