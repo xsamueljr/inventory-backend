1d53353f891227595d9601c3c5a3d7120899f2ae
xsamueljr <git@xsamueljr.anonaddy.me>
2025-05-16 19:37:48 +0200

    feat: user repository dynamically chosen based on env

--- users/infrastructure/fastapi/dependencies.py
+++ users/infrastructure/fastapi/dependencies.py
@@ -2,8 +2,11 @@ from functools import lru_cache
 
 from users.domain.user_repository import UserRepository
 from users.infrastructure.in_memory_user_repository import InMemoryUserRepository
-
+from shared.infrastructure.env import env
+from users.infrastructure.sqlite_user_repository import SQLiteUserRepository
 
 @lru_cache
 def get_user_repository() -> UserRepository:
+    if env.SQLITE_PATH:
+        return SQLiteUserRepository(env.SQLITE_PATH)
     return InMemoryUserRepository()
