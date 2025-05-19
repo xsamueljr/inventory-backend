efecd83db1e8402e36983f7695fb829e00fff9d4
xsamueljr <git@xsamueljr.anonaddy.me>
2025-05-19 13:19:27 +0200

    feat(logging): added logging for each usecase that requires it

--- emails/domain/product_created.py
+++ emails/domain/product_created.py
@@ -3,13 +3,16 @@ from products.domain.product import Product
 
 
 class ProductCreatedEmail(Email):
-    def __init__(self, product: Product) -> None:
+    def __init__(self, user_name: str, product: Product) -> None:
         subject = "Se ha creado un producto"
+        arrival_field = f"Fecha de llegada: {product.arriving_date}" \
+            if product.arriving_date else ""
 
         content = (
-            "Se acaba de crear un producto con la siguiente info:"
+            f"{user_name} acaba de crear un producto con la siguiente info:",
             f"- Modelo: {product.name}",
-            f"- Stock: {product.stock}"
+            f"- Stock: {product.stock}",
+            arrival_field
         )
         
         super().__init__(subject, "\n".join(content))
--- products/application/delete_by_id.py
+++ products/application/delete_by_id.py
@@ -1,9 +1,17 @@
+from auth.domain.logged_user_info import LoggedUserInfo
 from products.domain.product_repository import ProductRepository
+from shared.domain.logger import Logger
 
 
 class DeleteProductByIdUsecase:
-    def __init__(self, product_repository: ProductRepository) -> None:
+    def __init__(
+            self,
+            logger: Logger,
+            product_repository: ProductRepository
+    ) -> None:
+        self.__logger = logger
         self.__repo = product_repository
 
-    def run(self, id: str) -> None:
+    def run(self, user: LoggedUserInfo, id: str) -> None:
         self.__repo.delete(id)
+        self.__logger.info(f"{user.name} ha borrado el producto con ID {id}")
