208e7ee95ac58bf4a1321c007bea3832a3eb2c42
xsamueljr <git@xsamueljr.anonaddy.me>
2025-05-27 19:55:07 +0200

    refactor(entrypoint): separated the creation of the FastAPI app and the server startup in 2 files

--- main.py
+++ main.py
@@ -1,32 +1,10 @@
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
-from auth.infrastructure.fastapi.router import router as auth_router
-from products.infrastructure.fastapi.router import router as products_router
-from shared.infrastructure.env import ENV
-
-
-def create_app() -> FastAPI:
-    app = FastAPI()
-    app.add_middleware(
-        CORSMiddleware,
-        allow_origins=[str(ENV.FRONTEND_URL).rstrip("/")],
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
-    )
-
-    app.include_router(auth_router)
-    app.include_router(products_router)
-
-    return app
+from app import app
 
 
 def main():
-    app = create_app()
-
-    uvicorn.run(app)
+    uvicorn.run(app, host="0.0.0.0")
 
 
 if __name__ == "__main__":
