ba55f51db3500993c11e2dca6cce0bf2cbdc97f3
Confortcasa <confortcasa.dev@gmail.com>
2025-07-04 23:18:29 +0200

    feat: implemented activity domain

---
+++ activity/domain/record_repository.py
@@ -0,0 +1,16 @@
+from abc import ABC, abstractmethod
+from typing import List
+
+from activity.domain.record import Record
+
+
+class RecordRepository(ABC):
+
+    @abstractmethod
+    def save(self, record: Record) -> None: ...
+
+    @abstractmethod
+    def get_all(self, limit: int, offset: int) -> List[Record]: ...
+
+    @abstractmethod
+    def get_by_user_id(self, id: str, limit: int, offset: int) -> List[Record]: ...
