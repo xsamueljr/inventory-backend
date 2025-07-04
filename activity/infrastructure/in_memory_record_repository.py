f60f548ea37207bac8ef78e9a289f2ed46c3d6ad
xsamueljr <samueljiro.alfa@gmail.com>
2025-07-04 23:19:09 +0200

    feat: implemented a memory record repository, useful for testing

---
+++ activity/infrastructure/in_memory_record_repository.py
@@ -0,0 +1,22 @@
+from typing import Dict, List
+from activity.domain.record import Record
+from activity.domain.record_repository import RecordRepository
+
+
+class InMemoryRecordRepository(RecordRepository):
+
+    def __init__(self) -> None:
+        self.__records: Dict[str, Record] = {}
+
+    def save(self, record: Record) -> None:
+        self.__records[record.id] = record
+    
+    def get_all(self, limit: int, offset: int) -> List[Record]:
+        return list(self.__records.values())
+    
+    def get_by_user_id(self, id: str, limit: int, offset: int) -> List[Record]:
+        return [
+            record
+            for record in self.__records.values()
+            if record.user_id == id
+        ]
