04b37266c6c3f38e5a9bfd12854392dd254cd33c
xsamueljr <samueljiro.alfa@gmail.com>
2025-07-04 23:18:51 +0200

    feat: created a usecase to retrieve your own records

---
+++ activity/application/get_own_records.py
@@ -0,0 +1,19 @@
+from typing import List
+from activity.domain.record import Record
+from activity.domain.record_repository import RecordRepository
+from auth.domain.logged_user_info import LoggedUserInfo
+
+
+class GetOwnRecordsUseCase:
+    def __init__(
+        self,
+        repo: RecordRepository
+    ) -> None:
+        self.__repo = repo
+    
+    def run(self, user: LoggedUserInfo, limit: int, offset: int) -> List[Record]:
+        return self.__repo.get_by_user_id(
+            user.id,
+            limit,
+            offset
+        )
