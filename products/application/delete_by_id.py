7887aedb596be695a728165ef0dcbc44dc344bb7
xsamueljr <samueljiro.alfa@gmail.com>
2025-07-19 16:10:08 +0200

    feat: deleting a product now creates a record about it

--- products/application/delete_by_id.py
+++ products/application/delete_by_id.py
@@ -1,3 +1,7 @@
+from uuid import uuid4
+
+from activity.domain.record_repository import RecordRepository
+from activity.domain.record import Record, RecordKind
 from auth.domain.logged_user_info import LoggedUserInfo
 from products.domain.exceptions.product_not_found import ProductNotFoundException
 from products.domain.product_repository import ProductRepository
@@ -5,14 +9,24 @@ from shared.domain.logger import Logger
 
 
 class DeleteProductByIdUsecase:
-    def __init__(self, logger: Logger, product_repository: ProductRepository) -> None:
+    def __init__(self, logger: Logger, product_repository: ProductRepository, record_repository: RecordRepository) -> None:
         self.__logger = logger
         self.__repo = product_repository
+        self.__record_repo = record_repository
 
     def run(self, user: LoggedUserInfo, id: str) -> None:
         product = self.__repo.get_by_id(id)
         if product is None:
             raise ProductNotFoundException(id)
 
+        record = Record(
+            id=str(uuid4()),
+            kind=RecordKind.PRODUCT_DELETED,
+            user_id=user.id,
+            product_id=id,
+            amount=product.stock,
+        )
+
         self.__repo.delete(id)
+        self.__record_repo.save(record)
         self.__logger.info(f"{user.name} ha borrado el {product.name} (ID {id})")
