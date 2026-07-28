e2dcad4be22972d468b6a8972cd4d5d0f78194e8
xsamueljr <git@xsamueljr.anonaddy.me>
2026-07-28 19:39:00 +0200

    add error type to exception

--- shared/domain/exception.py
+++ shared/domain/exception.py
@@ -1,4 +1,16 @@
+from enum import Enum, auto
+
+
+class ErrorType(Enum):
+    NOT_FOUND = auto()
+    CONFLICT = auto()
+    UNAUTHORIZED = auto()
+    VALIDATION = auto()
+    INTERNAL = auto()
+
+
 class AppException(Exception):
-    def __init__(self, *args: object, is_operational: bool) -> None:
+    def __init__(self, *args: object, is_operational: bool, type: ErrorType) -> None:
         super().__init__(*args)
         self.is_operational = is_operational
+        self.type = type
