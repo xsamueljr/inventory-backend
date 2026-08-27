bbf9c9b11ca1f75ec5a5d541e1560b48447993d1
xsamueljr <git@xsamueljr.anonaddy.me>
2026-08-27 14:08:29 +0200

    add role to user to have admin operations later on

--- users/domain/user.py
+++ users/domain/user.py
@@ -1,5 +1,10 @@
 from dataclasses import dataclass
+from enum import Enum
 
+
+class UserRole(Enum):
+    USER = "user"
+    ADMIN = "admin"
 
 @dataclass
 class User:
@@ -8,3 +13,7 @@ class User:
     password: str
     shop_name: str
     location_id: int | None = None
+    role: UserRole = UserRole.USER
+
+    def is_admin(self) -> bool:
+        return self.role == UserRole.ADMIN
