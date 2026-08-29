23a5e39cd7b104d83b0eb2e987389d708589bd8a
xsamueljr <git@xsamueljr.anonaddy.me>
2026-08-29 16:30:46 +0200

    update logged user info type

--- auth/domain/logged_user_info.py
+++ auth/domain/logged_user_info.py
@@ -1,8 +1,20 @@
 from dataclasses import dataclass
 
+from users.domain.user import User
+
 
 @dataclass(frozen=True)
 class LoggedUserInfo:
     id: str
     name: str
     location_id: int | None = None
+    is_admin: bool = False
+
+    @classmethod
+    def from_domain(cls, user: User) -> "LoggedUserInfo":
+        return cls(
+            id=user.id,
+            name=user.username,
+            location_id=user.location_id,
+            is_admin=user.is_admin(),
+        )
--- core/infrastructure/fastapi/security.py
+++ core/infrastructure/fastapi/security.py
@@ -31,4 +31,4 @@ def get_current_user(
     if not user:
         raise credentials_exception
 
-    return LoggedUserInfo(user.id, user.username, user.location_id)
+    return LoggedUserInfo.from_domain(user)
